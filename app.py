fd9fbfeb16fe8af36ea78db98450e964bebf4c4e
Stella Odewumi <stellaodewumi@gmail.com>
2016-02-02 19:47:16 -0600

    Added Profile

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
-from flask import render_template
+import os
+
+from flask import Flask, render_template
+
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def index():
 	return render_template('index.html')
 
+PORT = int(os.environ.get("PORT", 5000))
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run(host='0.0.0.0', port=PORT)
